1951230546e94ff193fc098f8cbbd4f4a74e333c
David Charte <fdavidcl@outlook.com>
2014-03-21 13:52:07 +0100

    Suma dividida à la STD

--- src/suma.cpp
+++ src/suma.cpp
@@ -2,22 +2,43 @@
 
 #include <iostream>
 #include <vector>
+#include <numeric>
 using namespace std;
 typedef vector<int>::iterator vit;
 
+int suma_bucle(vit inicio, vit final) {
+    int suma = 0;
+    
+    for (vit i = inicio; i != final; ++i) {
+        suma += *i;
+    }
+
+    return suma;
+}
 
 int suma_dividida(vit inicio, vit final) {
     if (inicio == final)
-	return *inicio;
+        return *inicio;
     else {
-	vit mitad = inicio + (inicio-final)/2;
-	int suma_der = suma_dividida (inicio, mitad);
-	int suma_izq = suma_dividida (++mitad, final);
-	return suma_der + suma_izq;
+        vit mitad(inicio);
+        
+        // std::distance y std::advance son constantes para iteradores 
+        // "Random Access", lineales en otro caso
+        std::advance(mitad, std::distance(inicio, final)/2);
+        int suma_der = suma_dividida (inicio, mitad);
+        int suma_izq = mitad < final ? suma_dividida (++mitad, final) : 0;
+        return suma_der + suma_izq;
     }
 }
 
 
 int main() {
+    vector<int> ejemplo(20);
+
+    std::iota(ejemplo.begin(), ejemplo.end(), 1);
+
+    cout << suma_dividida(ejemplo.begin(), --ejemplo.end()) << 
+        " = " << suma_bucle(ejemplo.begin(), ejemplo.end()) << 
+        " = " << std::accumulate(ejemplo.begin(), ejemplo.end(), 0) << endl;
 }
 
