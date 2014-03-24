a5d141f44c71d667a1ae38c1e39f31c245f6140f
M42 <mromang08@gmail.com>
2014-03-24 16:18:35 +0100

    Késimo menor. Viendo el algoritmo inicial había pensado que quizá esto sería más eficiente. Ahora lo dudo bastante. La idea es hacer un quicksort parcial, usar un elemento como pivote y buscar sólo entre el tramo de lista que nos interesa. Pero aunque puede que tenga mejor caso medio (que tampoco lo sé), parece que tiene el mismo caso peor de O(n**2).
    Por otro lado, está escrito en lenguaje Ruby, pero dialecto C++. Vendría bien revisarlo y adaptarlo al lenguaje, pero al menos, que sirva como pseudocódigo.

---
+++ src/kesimomenor2.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+#encoding utf8
+
+# Creo que es eficiencia O(n**2) en el peor caso y O(n) en el medio.
+
+class Array
+  def kesimo (k)
+    pivote = self.first
+    
+    # Esto tiene eficiencia O(n).
+    # Partimos la lista en dos, sobre un pivote.
+    i = 1
+    j = size - 1
+    while i < j do
+      if self[i] < pivote
+        i++
+      elsif self[j] > pivote
+        j--
+      else
+        temp = self[i]
+        self[i] = self[j]
+        self[j] = temp
+      end
+    end
+
+    temp = self[i]
+    self[i] = self[0]
+    self[0] = temp
+
+    # Estudiamos si el pivote es el elemento buscado.
+    # Buscamos en el subarray correspondiente.
+    if i == k
+      return self[i]
+    elsif i < k
+      return self[i+1..size-1].kesimo(k-i-1)
+    else
+      return self[0..i-1].kesimo(k)
+    end
+  end
+end
