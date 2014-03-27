257a8efa62f44eb6993d7f8386da9cca9d0fe51c
David Charte <fdavidcl@outlook.com>
2014-03-27 01:44:29 +0100

    Explicación mayoritario, sin análisis aún

--- src/suma.rb
+++ src/suma.rb
@@ -2,11 +2,11 @@
 #encoding utf8
 
 class Array
-	def sum
-		return first if size < 2
+  def sum
+    return first if size < 2
 
-		self[0 .. size/2-1].sum + self[size/2 .. size - 1].sum
-	end
+    self[0 .. size/2-1].sum + self[size/2 .. size - 1].sum
+  end
 end
 
 if __FILE__ == $0
