82e52613cf692093dc02070b060b216dc93692d8
M42 <mromang08@gmail.com>
2014-04-02 23:21:39 +0200

    Implementación del késimo menor en Haskell. Completada y funcionando con la mediana de medianas.

--- src/kesimo_menor.hs
+++ src/kesimo_menor.hs
@@ -1,10 +1,13 @@
+import Data.List
+import Data.List.Split
+
 {-
   Caso medio lineal, caso peor cuadrático.
   Toma un pivote, parte la lista en tres partes y toma la parte del késimo elemento. 
  -}
 kesimo :: (Ord a) => [a] -> Int -> Maybe a
-kesimo []          _ = Nothing
-kesimo (pivote:xs) k
+kesimo [] _ = Nothing
+kesimo xs k
   | k < parte1 = kesimo menores k
   | k < parte2 = Just pivote 
   | otherwise  = kesimo mayores (k-parte2)
@@ -12,4 +15,14 @@ kesimo (pivote:xs) k
           mayores = filter (> pivote) xs
           iguales = filter (==pivote) xs
           parte1  = length menores
-          parte2  = parte1 + length iguales + 1 
+          parte2  = parte1 + (length iguales)
+          pivote  = medianOfMedians xs
+
+{-
+  Elección de la pseudomediana.
+  Partimos la lista en sublistas de 5 elementos, obtenemos la mediana y formamos nueva lista.
+ -}
+medianOfMedians :: (Ord a) => [a] -> a
+medianOfMedians [x] = x
+medianOfMedians xs  = medianOfMedians $ map median (chunksOf 5 xs)
+  where median xs = (sort xs) !! (div (length xs) 2)
